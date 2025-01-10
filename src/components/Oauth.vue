3e077bd351b7d95b8d49c3710bce49eca01f31bc
Mikhael as El <scorpiontai22@outlook.com>
2025-01-10 07:11:48 +0700

    Update Oauth.vue
    make easys indexedDB with localForage pattern in o auth 
    (need another page)

--- src/components/Oauth.vue
+++ src/components/Oauth.vue
@@ -1,71 +1,49 @@
-<script>
-import Cookies from 'js-cookie'
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+<template>
+<div>
+<p>sedang mengarahkan anda....</P>
+</div>
+</template>  
+      <script>
+        import Cookies from 'js-cookie'
+        import { onMounted } from 'vue'
+        import { useRoute } from 'vue-router'
+        import localforage from 'localforage'
 
-export default {
-    name: 'oauthGoogle',
+        export default {
+            name: 'oauthGoogle',
 
-    setup() {
-        const route = useRoute();
+            setup() {
+                const route = useRoute();
+                let encription = localforage.createInstance({
+                   name: "KeyDatabase"
 
-        onMounted(() => {
-            let encKey = route.query.encKey;
-            let ivKey = route.query.ivKey;
+            })
+            
+                onMounted(() => {
+                    const accessToken = route.query.accessToken;
+                const email = route.query.email
+                    if (accessToken) {
+                                const now = new Date()
+                                localStorage.setItem("oauth", accessToken);
+                                Cookies.set('tokenUser', email, { expires:  now.setTime(now.getTime() + (1 * 60 * 60 * 1000)), path: '/' })
+                        }
+                    let encKey = route.query.encKey;
+                    let ivKey = route.query.ivKey;
 
-            encKey = encKey.split(',').map(Number);
-            ivKey = ivKey.split(',').map(Number);
+                    if(encKey && ivKey){
+                    encKey = encKey.split(',').map(Number);
+                    ivKey = ivKey.split(',').map(Number);
+                    }
+                
+                    if (encKey && ivKey) {
+                        encription.setItem("encKey", encKey)
+                        encription.setItem("ivKey", ivKey)
+                    }
+                    setTimeout(() => {
+                        window.location.href = `http://localhost:8080/home`
+                    }, 5000);
 
-
-         
-            const accessToken = route.query.accessToken;
-            const email = route.query.email
-
-            if (accessToken) {
-                const now = new Date()
-                localStorage.setItem("oauth", accessToken);
-                Cookies.set('tokenUser', now.setTime(now.getTime() + (1 * 60 * 60 * 1000)), { expires: now, path: '/' });
-            }
-
-            if (encKey && ivKey) {
-                const request = indexedDB.open('KeyDatabase', 1);
-        
-        request.onerror = (event) => {
-            console.error('IndexedDB error:', event.target.error);
+                })
+            },
         };
-
-        request.onupgradeneeded = (event) => {
-            const db = event.target.result;
-            if (!db.objectStoreNames.contains('keys')) {
-                db.createObjectStore('keys', { keyPath: 'id' });
-            }
-        };
-
-        request.onsuccess = (event) => {
-            const db = event.target.result;
-            const transaction = db.transaction(['keys'], 'readwrite');
-            const store = transaction.objectStore('keys');
-
-            store.put({ id: 'encKey', value: encKey });
-            store.put({ id: 'ivKey', value: ivKey });
-
-            transaction.oncomplete = () => {
-                console.log('Keys stored successfully in IndexedDB');
-                window.location.href = `http://localhost:8080/home`;
-            };
-
-            transaction.onerror = (event) => {
-                console.error('Error:', event.target.error);
-            };
-        };
-                //please store this at indexedDb of encKey and ivKey
-            } else {
-                console.log('encKey or ivKey is missing.');
-            }
-        });
-
-    },
-};
-</script>
-
-<style scoped></style>
+        </script>
